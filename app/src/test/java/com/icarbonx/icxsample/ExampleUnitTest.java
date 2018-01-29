6c348f4b830a684bed87c120d7db8977f2e2fe85
fengfenkai <fengfenkai@icarbonx.com>
2018-01-29 18:57:34 +0800

    modify code

--- app/src/test/java/com/icarbonx/icxsample/ExampleUnitTest.java
+++ app/src/test/java/com/icarbonx/icxsample/ExampleUnitTest.java
@@ -13,5 +13,9 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+
+        String str = "icx_fit_project";
+        str = str.replace("_", "-");
+        System.out.print(str);
     }
 }
