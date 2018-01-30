1a6787c211ac72b955563126d5d8637db232445c
fengfenkai <fengfenkai@icarbonx.com>
2018-01-30 19:51:09 +0800

    add some file

---
+++ app/src/main/java/com/icarbonx/icxsample/app/activity/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.icarbonx.icxsample.app.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Author:  Kevin Feng
+ * Email:   fengfenkai@icarbonx.com
+ * Date:    2018/1/30
+ * Description:
+ */
+public class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
