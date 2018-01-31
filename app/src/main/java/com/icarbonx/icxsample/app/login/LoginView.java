5613962cdf6af0dbf755fd4914aa7bb7bc6b4933
fengfenkai <fengfenkai@icarbonx.com>
2018-01-31 13:26:38 +0800

    修改gradle

---
+++ app/src/main/java/com/icarbonx/icxsample/app/login/LoginView.java
@@ -0,0 +1,10 @@
+package com.icarbonx.icxsample.app.login;
+
+/**
+ * Author:  Kevin Feng
+ * Email:   fengfenkai@icarbonx.com
+ * Date:    2018/1/31
+ * Description:
+ */
+public interface LoginView {
+}
