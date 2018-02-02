4750ef809138eb79ce3f9a0465c827d66ec9af3c
fengfenkai <fengfenkai@icarbonx.com>
2018-02-02 18:29:10 +0800

    add human function

--- app/src/main/java/com/icarbonx/icxsample/MainActivity.java
+++ app/src/main/java/com/icarbonx/icxsample/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.test_random);
     }
 
 }
---
+++ app/src/main/java/com/icarbonx/icxsample/TestRandom.java
@@ -0,0 +1,32 @@
+package com.icarbonx.icxsample;
+
+import java.util.Random;
+
+/**
+ * Author:  Kevin Feng
+ * Email:   fengfenkai@icarbonx.com
+ * Date:    2018/2/2
+ * Description:
+ */
+public class TestRandom {
+
+    public TestRandom() {
+
+    }
+
+    public static final void main(String [] args) {
+//        Random random1 = new Random();
+//        Random random2 = new Random();
+//        int [][] arry = new int[100][2];
+//        for (int i = 0; i < 100; i ++) {
+//            arry[i][0] = random1.nextInt(500);
+//            arry[i][1] = random2.nextInt(500);
+//        }
+//        for (int i = 0; i < 100; i++) {
+//            System.out.println(arry[i][0] + " " + arry[i][1]);
+//        }
+
+        double d = 1 / Math.sqrt(2);
+        System.out.println(d);
+    }
+}
